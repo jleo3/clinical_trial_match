010661de8eb4f24e616006622617c904007d0c78
Michael Wenger <michaelwenger27@gmail.com>
2013-10-02 17:20:42 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClinicalTrialMatcher::Application.config.secret_key_base = '105f19a04947def9e27e372e25cf10074a676979362bbc8737f01a543036cfa46b77b3810900c28c23630ca2b6683b5c725d237721aebcd9eec3e455362fc305'
