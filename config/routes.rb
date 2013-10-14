1a7548e45cac1af313bbf49faf23111ef4bf6b5f
Michael Wenger <michaelwenger27@gmail.com>
2013-10-13 21:02:12 -0400

    added link to international trial matcher

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ClinicalTrialMatcher::Application.routes.draw do
   resources :trials
+  root 'trials#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
