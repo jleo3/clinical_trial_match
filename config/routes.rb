10ecc4da6db3cfbf920f47e1a0ce489534ffb307
Michael Wenger <michaelwenger27@gmail.com>
2013-10-26 10:01:08 -0400

    Added lat long for devise user. Changed html of index view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ ClinicalTrialMatcher::Application.routes.draw do
   resources :sites
   root 'trials#index'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025193743) do
+ActiveRecord::Schema.define(version: 20131026042315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20131025193743) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
