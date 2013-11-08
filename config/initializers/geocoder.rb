c8ec3780775dd275a9e3f399371c5a61c60a9862
Michael Wenger <michaelwenger27@gmail.com>
2013-11-07 21:55:14 -0500

    Switched back to YAML due to limites

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,7 @@
 Geocoder.configure(
 
   # geocoding service (see below for supported options):
-  :lookup => :google
+  :lookup => :yandex
 #  :google, :google_premier, :yandex
   # # to use an API key:
   # :api_key => "AIzaSyCs17R4Eav915EMHBRDQeTzlQZO6H57n_0",
