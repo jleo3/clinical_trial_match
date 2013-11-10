6d6a4b3b9bb115357ae0c160b3d538a0a65b7138
Michael Wenger <michaelwenger27@gmail.com>
2013-11-10 17:54:33 -0500

    Added devise confirmable to user registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
 
     def self.from_omniauth(auth)
