eadd02355e8443582b40f9f34eecaef07c73c7c6
Michael Wenger <michaelwenger27@gmail.com>
2013-11-10 15:26:12 -0500

    Updated config files for using send grid mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ ClinicalTrialMatcher::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000'}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ ClinicalTrialMatcher::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://shrouded-river-3637.herokuapp.com'}
 end
